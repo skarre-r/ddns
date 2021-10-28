3fb950335fe969062cfa51dda5e3f8fd48cb732f
skarre-r <pls.never@pm.me>
2021-10-28 21:09:11 +0200

    refactor: remove dedicated 'create_session' function in ddns script

--- ddns.py
+++ ddns.py
@@ -9,17 +9,9 @@ from urllib3.util.retry import Retry
 NJALLA_BASE_URL: str = "https://njal.la/update/"
 
 
-def create_session() -> requests.Session:
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount(prefix='https://', adapter=adapter)
-    return session
-
-
 def update_ddns() -> None:
     with open('conf.json', "r") as config_file:
-        with create_session() as session:
+        with requests.Session() as session:
             config: dict = json.load(config_file)
             domain: str
             subdomains: str
@@ -27,7 +19,7 @@ def update_ddns() -> None:
                 subdomain: str
                 key: str
                 for subdomain, key in subdomains.items():
-                    _domain = f"{subdomain}.{domain}"
+                    _domain: str = f"{subdomain}.{domain}"
                     url: str = f"{NJALLA_BASE_URL}?h={_domain}&k={key}&auto"
 
                     print(f"{_domain}: updating dns record...")
